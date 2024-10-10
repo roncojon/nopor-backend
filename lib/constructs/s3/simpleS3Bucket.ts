13c11119de11db1f1ecc55d3242b78112d3f8ff3
Ronald <rguillen@datamart.cl>
2024-10-10 15:10:20 -0400

    -ghn

--- lib/constructs/s3/simpleS3Bucket.ts
+++ lib/constructs/s3/simpleS3Bucket.ts
@@ -5,15 +5,13 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 export class SimpleS3Bucket extends Construct {
   public readonly bucket: s3.Bucket;
 
-  constructor(scope: Construct, id: string/* , bucketName: string */) {  // Accept bucketName as a parameter
+  constructor(scope: Construct, id: string, bucketName: string) {  // Accept bucketName as a parameter
     super(scope, id);
 
     // Create an S3 bucket with a dynamic name
-    this.bucket = new s3.Bucket(this, 'SimpleS3BucketInstance0Nopor0', {
-      bucketName: `nopor-bucket-${cdk.Stack.of(this).region}-${cdk.Stack.of(this).account}`,
-      versioned: false
+    this.bucket = new s3.Bucket(this, id, {   // Use 'id' as the logical ID
+      versioned: false,
+      bucketName: bucketName  // Use the dynamic bucketName provided
     });
-    
   }
 }
-
