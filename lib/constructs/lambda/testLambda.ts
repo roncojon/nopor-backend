bb3d0681ebd0142b6785f6eb4a205605aeeb9b33
Ronald <rguillen@datamart.cl>
2024-10-11 19:40:18 -0400

    gb

--- lib/constructs/lambda/testLambda.ts
+++ lib/constructs/lambda/testLambda.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as path from 'path';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class MyLambdaFunction extends Construct {
   public readonly lambdaFunction: lambda.Function;
@@ -9,11 +10,17 @@ export class MyLambdaFunction extends Construct {
   constructor(scope: Construct, id: string, lambdaName: string) {
     super(scope, id);
 
-    this.lambdaFunction = new lambda.Function(this, lambdaName, {
-      runtime: lambda.Runtime.NODEJS_20_X,
-      // code: lambda.Code.fromAsset(path.join(__dirname, './lambdas/createNoporLambdav2')), // Path to your lambda folder
-      code: lambda.Code.fromAsset(path.join(__dirname, '../../lambdas/createNoporLambdav2')),
-      handler: 'index.handler', // File name and exported function name
+    // this.lambdaFunction = new lambda.Function(this, lambdaName, {
+    //   runtime: lambda.Runtime.NODEJS_20_X,
+    //   code: lambda.Code.fromAsset(path.join(__dirname, './lambdas/createNoporLambdav2')), // Path to your lambda folder
+    //   // code: lambda.Code.fromAsset(path.join(__dirname, '../../lambdas/createNoporLambdav2')),
+    //   handler: 'index.handler', // File name and exported function name
+    // });
+    this.lambdaFunction = new NodejsFunction(this, 'MyLambdaFunction', {
+      entry: path.join(__dirname, './lambdas/createNoporLambdav2/index.js'), // or index.ts for TypeScript
+      // entry: path.join(__dirname, '../../lambdas/createNoporLambdav2/index.js'), // or index.ts for TypeScript
+      handler: 'handler', // the exported handler function
+      runtime: lambda.Runtime.NODEJS_18_X,
     });
   }
 }
