a27c101a1647891a3242cf5b6e058c02f13e2b19
Ronald <rguillen@datamart.cl>
2024-10-11 18:58:40 -0400

    -split lambdas to separate files

--- lib/constructs/lambda/testLambda.ts
+++ lib/constructs/lambda/testLambda.ts
@@ -1,24 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as path from 'path';
 
 export class MyLambdaFunction extends Construct {
   public readonly lambdaFunction: lambda.Function;
 
-  constructor(scope: Construct, id: string, lambdaName:string) {
+  constructor(scope: Construct, id: string, lambdaName: string) {
     super(scope, id);
 
     this.lambdaFunction = new lambda.Function(this, lambdaName, {
-      runtime: lambda.Runtime.NODEJS_18_X,
-      code: lambda.Code.fromInline(`
-        exports.handler = async (event) => {
-          return {
-            statusCode: 200,
-            body: JSON.stringify({ message: 'Hello from Lambda!' }),
-          };
-        };
-      `),
-      handler: 'index.handler',
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset(path.join(__dirname, './lambdas/createNoporLambdav2')), // Path to your lambda folder
+      handler: 'index.handler', // File name and exported function name
     });
   }
 }
