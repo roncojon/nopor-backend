7a3179e69f93f3a4a6fbb329fefd5d0dd2c360d3
Ronald <rguillen@datamart.cl>
2024-10-12 00:05:17 -0400

    -fix paths for lambdas

--- lib/constructs/apigateway/testApiGateway.ts
+++ lib/constructs/apigateway/testApiGateway.ts
@@ -1,7 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import { TestLambdaFunction } from '../lambdas/createNoporData/testLambdaConstruct';
+import { TestLambdaFunction } from '../lambdas/test/testLambdaConstruct';
 
 export class MyApiGateway extends Construct {
   constructor(scope: Construct, id: string, apiName:string) {
